65272d21935417fb7a784667054b7d7220474e7b
Apoorva Chauhan <apoorva1142318@gmail.com>
2022-06-29 15:58:06 -0400

    Docker-GraphqL project

---
+++ Producs/products/app.js
@@ -0,0 +1,12 @@
+// products/app.js
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.json([{
+  id: "1",
+  name: 'Avengers - infinity war',
+  description: 'a Blue ray movie'
+}]))
+app.listen(port, () => console.log(`Example app listening on port port!`))
